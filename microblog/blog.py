0fcbd9cffb0d03975ad55f584fcbd60692fdc266
apple <apple@apples-iMac.local>
2023-04-16 13:28:32 +0300

    commit posts to db and retrieve

--- microblog/blog.py
+++ microblog/blog.py
@@ -2,13 +2,17 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from core.utils import get_db
 from . import service
-
+from .schemas import PostCreate
 
 router = APIRouter()
 
 
 @router.get("/")
 def post_list(db: Session = Depends(get_db)):
-    #print(db)
-    posts = service.get_post_list(db)
-    return posts
+    # print(db)
+    return service.get_post_list(db)
+
+
+@router.post("/")
+def post_list(item: PostCreate, db: Session = Depends(get_db)):
+    return service.create_post(db, item)
--- microblog/schemas.py
+++ microblog/schemas.py
@@ -6,11 +6,15 @@ from pydantic import BaseModel
 class PostBase(BaseModel):
     title: str
     text: str
-
-
-class PostList(BaseModel):
-    id: int
     date: datetime
 
     class Config:
         orm_mode = True
+
+
+class PostList(PostBase):
+    id: int
+
+
+class PostCreate(PostBase):
+    pass
