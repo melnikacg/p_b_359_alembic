4d7e8aeb36167c9ca9e8950d2e1d5261182a2240
apple <apple@apples-iMac.local>
2023-05-01 09:46:49 +0300

    before problem

--- user/models.py
+++ user/models.py
@@ -1,6 +1,14 @@
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+
 from core.db import Base
 
+
+#class UserTable(Base, SQLAlchemyBaseUserTable):
+#    __tablename__ = "user_table"
+    #uvicorn main:app --reloadpass
+#    name = Column(String, unique=True)
+#    date = Column(DateTime)
 
 class User(Base):
     __tablename__ = "user"
